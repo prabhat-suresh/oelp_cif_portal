88db6afb84a22f625206dcddf451303c217558fe
Kshitij.G <112101025@smail.iitpkd.ac.in>
2024-02-20 15:53:39 +0530

    basic schema and signup route

--- cif-portal/src/app/api/createuser/route.ts
+++ cif-portal/src/app/api/createuser/route.ts
@@ -1,1 +1,57 @@
-import mongoose from "mongoose";
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const request_body = await request.json();
+
+    // log
+    console.log(request_body);
+    const { username, email, password, mobile, department } = request_body;
+
+    // check if user already exists
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 },
+      );
+    }
+    // hash password
+    const salt = await bcryptjs.genSalt(12);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // create user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      mobile: mobile,
+      department: department
+    });
+    // save the user
+    const savedUser = await newUser.save();
+
+    // log
+    console.log(savedUser);
+
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      status: 201,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+// curl command to check
+//   curl -X POST \
+//   -H "Content-Type: application/json" \
+//   -d '{"username":"test","email":"test@test.com","password":"pass"}' \
+//   http://localhost:3000/api/createuser
--- cif-portal/src/dbConfig/dbConfig.ts
+++ cif-portal/src/dbConfig/dbConfig.ts
@@ -1,21 +1,19 @@
-import  mongoose from "mongoose";
-const mongoDBURI="mongodb+srv://oelp:<password>@cluster0.rypu0hh.mongodb.net/?retryWrites=true&w=majority"
-export async function connect(){
-    try {
-        mongoose.connect(process.env.MONGO_URI!);
-        const connection = mongoose.connection;
+import mongoose from "mongoose";
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_DB_URI!);
+    const connection = mongoose.connection;
 
-        connection.on('connected', () => {
-            console.log('MongoDB has been connected successfully');
-        })
+    connection.on("connected", () => {
+      console.log("MongoDB has been connected successfully");
+    });
 
-        connection.on('error', (err) => {
-            console.log('Something went wrong, please check MongoDB' + err);
-            process.exit();
-        })
-    } catch (error){
-        console.log('Something went wrong!');
-        console.log(error);
-    }
-
+    connection.on("error", (err) => {
+      console.log("Something went wrong, please check MongoDB" + err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went wrong!");
+    console.log(error);
+  }
 }
