b17e003c227f772ec8a70ce8cf20084b42f9aa2a
Prabhat Suresh <prabhatsuresh5@gmail.com>
2024-04-16 19:20:19 +0530

    added component for equipment details

--- cif-portal/src/app/equipments/page.tsx
+++ cif-portal/src/app/equipments/page.tsx
@@ -1,4 +1,13 @@
 "use client"
+import React from "react"
+import equipment from "@/components/equipment"
+import Equipment from "@/models/equipmentModel";
+
 export default function Equipments() {
-    return <img src={"/TCS PS_0.jpg"} />
+    const equipDetails = { equipName: "kdfhbadksfj", imageUrl: "/TCS PS_0.jpg", descUrl: "./eqipment/1" };
+    return (
+        <div>
+            <Equipment props={equipDetails} />
+        </div>
+    );
 }
--- cif-portal/src/components/equipment.tsx
+++ cif-portal/src/components/equipment.tsx
@@ -1,25 +1,25 @@
-// "use client"
-// import React from 'react';
-// export default function equipment({equipDetails}){
-//   return <div>
-//       h2{equipDetails.name}
-//       <a href={equipDetails.imageUrl}>link</a>
-//       <a href={equipDetails.descUrl}>link</a>
-//   </div>
-// }
-import React from "react";
-
-interface EquipmentProps {
-  equipDetails: Record<string, any>; // JSON object type
+"use client"
+import React from 'react';
+export default function equipment(props: { equipName: string, imageUrl: string, descUrl: string }) {
+  return <div>
+    h2{props.equipName}
+    <img src={props.imageUrl} alt="" />
+    <a href={props.descUrl}> Book Equipment </a>
+  </div>
 }
+// import React from "react";
 
-const Equipment: React.FC<EquipmentProps> = ({ equipDetails }) => {
-  return (
-    <div>
-      <h2>{equipDetails.name}</h2>
-      <a href={equipDetails.imageUrl}>Image Link</a>
-      <a href={equipDetails.descUrl}>Description Link</a>
-    </div>
-  );
-};
-export default Equipment;
+// interface EquipmentProps {
+//   equipDetails: Record<string, any>; // JSON object type
+// }
+
+// const Equipment: React.FC<EquipmentProps> = ({ equipDetails }) => {
+//   return (
+//     <div>
+//       <h2>{equipDetails.name}</h2>
+//       <a href={equipDetails.imageUrl}>Image Link</a>
+//       <a href={equipDetails.descUrl}>Description Link</a>
+//     </div>
+//   );
+// };
+// export default Equipment;
