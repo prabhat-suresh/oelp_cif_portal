ba44859f9cf6375ee2888892c00db3c5fbee7c56
prabhat-suresh <prabhatsuresh5@gmail.com>
2024-05-11 22:50:12 +0530

    moved gitignore to parent directory

--- cif-portal/src/app/equipments/page.tsx
+++ cif-portal/src/app/equipments/page.tsx
@@ -1,10 +1,18 @@
-"use client"
+// "use client"
 import React from "react"
 import Equipment1 from "@/components/equipment"
 // import Equipment from "@/models/equipmentModel";
+import { GET } from "@/app/api/equipment/all/route"
 
-export default function Equipments() {
+export default async function Equipments() {
     const equipDetails = { equipName: "kdfhbadksfj", imageUrl: "/TCS PS_0.jpg", descUrl: "./equipments/equipment/1" };
+    try {
+        const res = await fetch('http://localhost:3000/api/equipment/all')
+        console.log(res)
+    }
+    catch (err) {
+        console.log("API Not working")
+    }
     return (
         <div>
             <Equipment1 {...equipDetails} />
