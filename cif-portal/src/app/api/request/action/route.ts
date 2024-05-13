9defa058baa94728fb31fa6d518b739f9b6c12e8
prabhat-suresh <prabhatsuresh5@gmail.com>
2024-05-13 15:59:13 +0530

    added api for approving requests

--- cif-portal/src/app/api/request/action/route.ts
+++ cif-portal/src/app/api/request/action/route.ts
@@ -11,17 +11,22 @@ export async function POST(req: NextRequest, res: NextResponse) {
         const { email, requestID, action }: any = req_body;
         // just check if the email can take action
 
-        const request_row = await Request.findOne({ requestID });
         const user_row = await User.findOne({ email });
         if (!user_row) {
             return NextResponse.json({ "status": 500, "message": "Internal server error" });
         }
         const user_role = user_row.role;
-        if (user_role != "faculty") {
+        if (user_role == "faculty") {
+            const request_row = await Request.findOneAndUpdate({ requestID }, { paApproval: action });
+        }
+        else if (user_role == "staff") {
+            const request_row = await Request.findOneAndUpdate({ requestID }, { staffApproval: action });
+        }
+        else {
             return NextResponse.json({ "status": 400, "message": "User does not have authorization." });
         }
 
-
+        return NextResponse.json({ "status": 200, "message": "Request Approved" });
 
     } catch (error: any) {
         return NextResponse.json({ "status": 500, "error": error.message })
